cfb4d2ca2389eda745e5a9cc46397e2fc4ae63c8
vasiioandre <vasi_ioandre@yahoo.com>
2024-05-23 18:58:57 +0300

    Explore Stubs

---
+++ src/test/java/com/example/mockito/mockito_demo/business/SomeBusinessImplTest.java
@@ -0,0 +1,26 @@
+package com.example.mockito.mockito_demo.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessImplTest {
+
+	@Test
+	void findTheGreatestFromAllData_basicScenario() {
+		DataServiceStub dataServiceStub = new DataServiceStub();
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub);
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(25, result);
+	}
+
+}
+
+class DataServiceStub implements DataService {
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {25, 15, 5};
+	}
+	
+}
